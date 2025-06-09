268b42daa36fdec0a8c2782b9a32c10302e61c5b
97mams <97mams@gmail.com>
2025-06-08 23:43:23 -0700

    done

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ import { GoogleGenAI } from "@google/genai"
 
 const apiKey = process.env.GEMINI_API_KEY
 
-const ai = new GoogleGenAI({})
+const ai = new GoogleGenAI({apiKey: apiKey})
 
-console.log(apiKey);
+async function main(){
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-falsh",
+    contents: "Expliquez comment l'IA fonctionne en quelques mots"
+  })
+  console.log(response.text)
+}
+
+main()
