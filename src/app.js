b219984a98724ebd392bf357e15be42fbfd5d1c4
97mams <97mams@gmail.com>
2025-06-13 07:17:58 -0700

    add realine

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
 import { GoogleGenAI } from "@google/genai"
+import { debug } from "node:console"
+import readline from "node:readline"
 
 const apiKey = process.env.GEMINI_API_KEY
 
 const ai = new GoogleGenAI({apiKey: apiKey})
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+
 async function main(){
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: "prompte"
+    contents: "salut gimini"
   })
   console.log(response.text)
 }
 
 main()
+rl.question("Entrez votre prompte: \n \x1b[32m + \x1b[0m", prompt => {
+  rl.close()
+})
